e95ace26dfdcef7e954bae919a6c38d1d509f51a
Nick Aceves <nickaceves@gmail.com>
2014-07-02 20:02:20 -0700

    Require.js mostly works now :)

--- scripts/asteroids.js
+++ scripts/asteroids.js
@@ -1,171 +1,166 @@
-var ship = { x: 0, y: 0, rot: 0, thrust: false, dx: 0, dy: 0 };
+define(['./config'], function(config) {
+  var ship = { x: 0, y: 0, rot: 0, thrust: false, dx: 0, dy: 0 };
 
-var keys = {};
+  var keys = {};
 
+  var renderingContext = null;
 
-var renderingContext = null;
+  var frameTime = null;
 
-var frameTime = null;
+  var simTime = null;
 
-var simTime = null;
+  function updatePosition(entity) {
+    entity.x += entity.dx;
+    entity.y += entity.dy;
+    
+    // TODO: global width and height that aren't on canvas
+    var w = renderingContext.canvas.width;
+    var h = renderingContext.canvas.height;
 
-var SIM_FPS = 20;
-var SIM_DELTA_SEC = 1.0 / SIM_FPS;
-var SIM_STEP_MS = 1000.0 / SIM_FPS;
-var SHIP_WIDTH = 20;
-var SHIP_LEN = 20;
-var KEYS = { LEFT: 39, RIGHT: 37, UP: 38, DOWN: 40 }
-var SHIP_ROT_SPEED = 2;
-
-function updatePosition(entity) {
-  entity.x += entity.dx;
-  entity.y += entity.dy;
-  
-  // TODO: global width and height that aren't on canvas
-  var w = renderingContext.canvas.width;
-  var h = renderingContext.canvas.height;
-
-  while (entity.x > w) { entity.x -= w; }
-  while (entity.x < 0) { entity.x += w; } 
-  while (entity.y > h) { entity.y -= h; }
-  while (entity.y < 0) { entity.y += h; }
-}
-
-function resetGame(w, h) {
-  ship.x = w / 2;
-  ship.y = h / 2;
-  ship.rot = 0;
-}
-
-function clearCanvas(ctx) {
-  ctx.fillStyle = '#000000';
-  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-}
-
-function path(ctx) {
-  ctx.beginPath();
-  ctx.moveTo(arguments[1][0], arguments[1][1]);
-  for (var i = 2; i < arguments.length; i++) {
-    ctx.lineTo(arguments[i][0], arguments[i][1]);
+    while (entity.x > w) { entity.x -= w; }
+    while (entity.x < 0) { entity.x += w; } 
+    while (entity.y > h) { entity.y -= h; }
+    while (entity.y < 0) { entity.y += h; }
   }
-  ctx.closePath();
-}
 
-function renderShip(ctx) {
-  ctx.save();
-  ctx.translate(ship.x, ship.y);
-  ctx.rotate(ship.rot);
-  ctx.translate(0, -SHIP_LEN*0.3);
+  function resetGame(w, h) {
+    ship.x = w / 2;
+    ship.y = h / 2;
+    ship.rot = 0;
+  }
 
-  ctx.fillStyle = 'red';
-  path(ctx, [0, SHIP_LEN], [-SHIP_WIDTH/2, 0], [SHIP_WIDTH/2, 0]);
-  ctx.fill();
+  function clearCanvas(ctx) {
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  }
 
-  if (ship.thrust) {
-    ctx.fillStyle = 'yellow';
-    path(ctx, [0, -SHIP_LEN/3], [-SHIP_WIDTH/4, 0], [SHIP_WIDTH/4, 0]);
+  function path(ctx) {
+    ctx.beginPath();
+    ctx.moveTo(arguments[1][0], arguments[1][1]);
+    for (var i = 2; i < arguments.length; i++) {
+      ctx.lineTo(arguments[i][0], arguments[i][1]);
+    }
+    ctx.closePath();
+  }
+
+  function renderShip(ctx) {
+    var SHIP_LEN = 22;
+    var SHIP_WIDTH = 20;
+
+    ctx.save();
+    ctx.translate(ship.x, ship.y);
+    ctx.rotate(ship.rot);
+    ctx.translate(0, -SHIP_LEN*0.3);
+
+    ctx.fillStyle = 'red';
+    path(ctx, [0, SHIP_LEN], [-SHIP_WIDTH/2, 0], [SHIP_WIDTH/2, 0]);
     ctx.fill();
-  }
 
-  ctx.restore();
-}
-
-function updateSim() {
-  if (keys[KEYS.LEFT]) {
-    ship.rot -= SHIP_ROT_SPEED * SIM_DELTA_SEC;
-  }
-  
-  if (keys[KEYS.RIGHT]) {
-    ship.rot += SHIP_ROT_SPEED * SIM_DELTA_SEC;
-  }
-
-  ship.thrust = keys[KEYS.UP];
-  
-  if (ship.thrust) {
-    ship.dy += Math.cos(ship.rot);
-    ship.dx -= Math.sin(ship.rot);
-  }
-
-  updatePosition(ship);
-}
-
-function everyNSeconds(n, callback) {
-  if (simTime % (n * 1000) < SIM_STEP_MS) {
-    callback();
-  }
-}
-
-function renderFrame(ctx) {
-  clearCanvas(ctx);
-  renderShip(ctx);
-}
-
-function update(time, delta) {
-  if (simTime == null) {
-    simTime = time;
-    return;
-  }
-
-  while (simTime + SIM_STEP_MS < time) {
-    simTime += SIM_STEP_MS;
-    updateSim();
-  }
-
-  renderFrame(renderingContext);
-}
-
-function debugReplacer(key, val) {
-  return (val && val.toFixed) ? Number(val.toFixed(3)) : val;
-};
-
-function getDebugString(delta) {
-  return ['keys: ' + JSON.stringify(keys, debugReplacer),
-      'delta: ' + delta, 
-      'fps: ' + Math.round(1000 / delta),
-      'ship: ' + JSON.stringify(ship, debugReplacer)
-    ].join('<br/>');
-}
-
-
-(function main() {
-  document.body.addEventListener('keydown', function(event) { keys[event.which] = true; });
-  document.body.addEventListener('keyup', function(event) { keys[event.which] = false; });
-
-  var debugConsole = document.createElement('div');
-  debugConsole.style.width = '100%';
-  debugConsole.style.wordWrap = 'break-word';
-
-  var prevFrameTime = null;
-  var prevDebugUpdate = 0;
-  window.requestAnimationFrame(function mainLoop(time) {
-    if (prevFrameTime == null) {
-      prevFrameTime = time;
-    }
-    else {
-      var delta = time - prevFrameTime;
-      update(time, time - prevFrameTime);
-      prevFrameTime = time;
+    if (ship.thrust) {
+      ctx.fillStyle = 'yellow';
+      path(ctx, [0, -SHIP_LEN/3], [-SHIP_WIDTH/4, 0], [SHIP_WIDTH/4, 0]);
+      ctx.fill();
     }
 
-    if (time - prevDebugUpdate > 50) {
-      prevDebugUpdate = time;
-      debugConsole.innerHTML = getDebugString(delta);
+    ctx.restore();
+  }
+
+  function updateSim() {
+    if (keys[config.KEYS.LEFT]) {
+      ship.rot -= config.SHIP_ROTATE_SPEED * config.SIM_DELTA_SEC;
+    }
+    
+    if (keys[config.KEYS.RIGHT]) {
+      ship.rot += config.SHIP_ROTATE_SPEED * config.SIM_DELTA_SEC;
     }
 
-    window.requestAnimationFrame(mainLoop);
-  });
+    ship.thrust = keys[config.KEYS.UP];
+    
+    if (ship.thrust) {
+      ship.dy += Math.cos(ship.rot);
+      ship.dx -= Math.sin(ship.rot);
+    }
 
-  var canvas = document.createElement('canvas');
-  canvas.width = 600;
-  canvas.height = canvas.width * (9/16);
-  document.body.appendChild(canvas);
-  renderingContext = canvas.getContext('2d');
+    updatePosition(ship);
+  }
 
-  // flip canvas so that 0, 0 is upper left corner
-  renderingContext.translate(0, canvas.height);
-  renderingContext.scale(1, -1);
+  function renderFrame(ctx) {
+    clearCanvas(ctx);
+    renderShip(ctx);
+  }
 
-  document.body.appendChild(debugConsole);
+  function update(time, delta) {
+    if (simTime == null) {
+      simTime = time;
+      return;
+    }
 
-  resetGame(canvas.width, canvas.height);
-})();
+    while (simTime + config.SIM_DELTA_MS < time) {
+      simTime += config.SIM_DELTA_MS;
+      updateSim();
+    }
+
+    renderFrame(renderingContext);
+  }
+
+  function debugReplacer(key, val) {
+    return (val && val.toFixed) ? Number(val.toFixed(3)) : val;
+  };
+
+  function getDebugString(delta) {
+    return ['keys: ' + JSON.stringify(keys, debugReplacer),
+        'delta: ' + delta, 
+        'fps: ' + Math.round(1000 / delta),
+        'ship: ' + JSON.stringify(ship, debugReplacer)
+      ].join('<br/>');
+  }
+
+
+  (function main() {
+    document.body.addEventListener('keydown', function(event) { keys[event.which] = true; });
+    document.body.addEventListener('keyup', function(event) { keys[event.which] = false; });
+
+    var debugConsole = document.createElement('div');
+    debugConsole.style.width = '100%';
+    debugConsole.style.wordWrap = 'break-word';
+
+    var prevFrameTime = null;
+    var prevDebugUpdate = 0;
+    window.requestAnimationFrame(function mainLoop(time) {
+      if (prevFrameTime == null) {
+        prevFrameTime = time;
+      }
+      else {
+        var delta = time - prevFrameTime;
+        update(time, time - prevFrameTime);
+        prevFrameTime = time;
+      }
+
+      if (time - prevDebugUpdate > 50) {
+        prevDebugUpdate = time;
+        debugConsole.innerHTML = getDebugString(delta);
+      }
+
+      window.requestAnimationFrame(mainLoop);
+    });
+
+    var canvas = document.createElement('canvas');
+    canvas.width = 600;
+    canvas.height = canvas.width * (9/16);
+    document.body.appendChild(canvas);
+    renderingContext = canvas.getContext('2d');
+
+    // flip canvas so that 0, 0 is upper left corner
+    renderingContext.translate(0, canvas.height);
+    renderingContext.scale(1, -1);
+
+    document.body.appendChild(debugConsole);
+
+    resetGame(canvas.width, canvas.height);
+  })();
+
+  return {
+    start: function() {
+    }
+  };
+}); // define
---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+define({
+  SIM_FPS: 20,               // How many sim frames per second.
+  SIM_DELTA_SEC: 1.0 / 20,   // Time in seconds between each sim frame.
+  SIM_DELTA_MS: 1000.0 / 20, // Time in milliseconds between each sim frame.
+  
+  KEYS: { LEFT: 39, RIGHT: 37, UP: 38, DOWN: 40 },
+  
+  SHIP_ROTATE_SPEED: 2      // Radians/sec.
+});
